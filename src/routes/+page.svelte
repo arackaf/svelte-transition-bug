71b1c032e1fcff7bda6027eb5df7436f5972fb38
Adam Rackis <adamrackis@hotmail.com>
2024-09-24 22:48:28 -0500

    add tailwind - repro - problem was type="button" is needed :/

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,43 @@
 <script lang="ts">
   import { enhance } from "$app/forms";
+  import { fade } from "svelte/transition";
+  import Item from "./Item.svelte";
 
   let items = $state<any[]>([]);
+  let search = $state(1);
 
   function executeSearch({ cancel, formData: data }: any) {
     cancel();
 
+    search++;
     items = Array.from({ length: 30 }).map((_, idx) => ({
       id: idx + 1,
       name: "Item " + idx + 1,
     }));
   }
+
+  function remove(toRemove: any) {
+    items = items.filter((item) => item.id != toRemove.id);
+  }
 </script>
 
-<form method="post" use:enhance={executeSearch}>
-  <button>Search</button>
-  {#each items as item}
-    <div>{item.name}</div>
-  {/each}
-</form>
+<div class="p-6">
+  <form method="post" use:enhance={executeSearch}>
+    <div class="flex flex-col gap-3">
+      <div>
+        <button class="border p-3">Search</button>
+      </div>
+      <div>
+        {#if items.length}
+          {#key search}
+            <div in:fade|local out:fade|local class="flex flex-col min-w-0">
+              {#each items as item (item.id)}
+                <Item {item} {remove} />
+              {/each}
+            </div>
+          {/key}
+        {/if}
+      </div>
+    </div>
+  </form>
+</div>
---
+++ src/routes/Item.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+
+  let { item, remove }: any = $props();
+</script>
+
+<div out:fade|local class="flex gap-3 p-3">
+  <span>{item.name}</span>
+  <button onclick={() => remove(item)}>Remove</button>
+</div>
