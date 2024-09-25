0e9cff3eb75ecf55bcb481b9fd2eece7622e1c25
Adam Rackis <adamrackis@hotmail.com>
2024-09-24 22:23:43 -0500

    start

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { enhance } from "$app/forms";
+
+  let items = $state<any[]>([]);
+
+  function executeSearch({ cancel, formData: data }: any) {
+    cancel();
+
+    items = Array.from({ length: 30 }).map((_, idx) => ({
+      id: idx + 1,
+      name: "Item " + idx + 1,
+    }));
+  }
+</script>
+
+<form method="post" use:enhance={executeSearch}>
+  <button>Search</button>
+  {#each items as item}
+    <div>{item.name}</div>
+  {/each}
+</form>
